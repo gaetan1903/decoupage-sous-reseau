5561798f58fe49ee7d8e6ddfc86a71b27999a93d
Landry Manankoraisina RASENDRANIRINA <landry.manankoraisina.rasendranirina@esti.mg>
2019-10-13 10:36:11 +0300

    Bug de masque fixé et ajout de quelques commentaires

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 '''
-    script permettant de decouper une addresse IP en sous reseaux en fonction
-        en  de nombre de departements
+    Script permettant de decouper une addresse IP en sous reseaux en fonction
+    de nombre de sous réseaux.
 '''
-import re
+import re #Module d'expression régulière en python
 import math
 
-
+#Fonction de conversion de binaire en décimal
 def base2todec(bin):
     cnv = 0
     n = 0
@@ -14,7 +14,7 @@ def base2todec(bin):
         n += 1
     return cnv
 
-
+#Fonction qui convertit le dernier octet de  masque en binaire
 def maskIso(mask):
     bt = list('00000000')
     val = mask % 8
@@ -22,40 +22,42 @@ def maskIso(mask):
         bt[i] = '1'
     return f"255.255.255.{base2todec(''.join(bt))}"
 
-    return ''
-
 
 ipd = ''
 while not re.match(r'^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$', ipd):
-    ipd = input("Entrer addresse réseau:  ")
+    ipd = input("Entrer l'addresse réseau >>>  ")
 
 nbSr = ''
 while not re.match(r'\d', nbSr):
-    nbSr = input("Entrer nombre de sous Réseaux: ")
+    nbSr = input("Entrer le nombre de sous réseau >>> ")
 
 sR = {}
 for i in range(int(nbSr)):
-    name = input(f"Entrer le nom du sous réseaux {i+1}: ")
+    name = input(f"Entrer le nom du sous réseau {i+1}: ")
     nbrPC = input(f"Entrer le nombre de PC: ")
+    print("")
     sR[name] = int(nbrPC)
 
-sR = {k: v for k, v in sorted(sR.items(), key=lambda kv: kv[1], reverse=True)}
+sR = {k: v for k, v in sorted(sR.items(), key=lambda kv: kv[1], reverse=True)}#Triage par plus grand nombre de PC par sous-réseau d'où reverse.
 sRdec = {}
 lastIp = ipd
 for nom, nbr in sR.items():
-    n = int(math.log2(nbr)) +  1
-    newmask = 32 - n
+    n_s = math.log2(nbr+2) #On cherche d'abord n.
+    if n_s - int(n_s) == 0: 
+        n = int(n_s)
+    else: 
+        n = int(n_s) + 1
+    newmask = 32 - n #La formule de nouveau masque des sous réseaux.
     ipsr = lastIp
     sRdec[nom] = (ipsr, nbr, (newmask, maskIso(newmask)))
     lastIp = lastIp.split('.')
     lastIp[-1] = str(int(lastIp[-1]) + 2**n)
     lastIp = '.'.join(lastIp)
 
-for key, val in sRdec.items():
+for key, val in sRdec.items(): #Affichage des sous réseaux.
     print(f"""
         {key}: {val[1]} PCs
-
+        ---------------------------------------------
         Adresse réseau: {val[0]}/{val[2][0]}
         Masque sous réseau: {val[2][1]}
     """)
-
