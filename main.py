3e8069e6827f7dec01f2dd7837a105b71e350281
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2019-10-24 15:36:59 +0300

    Lever une exception en cas de nombre de PC trop grand pour un reseaux de /24

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 '''
-    Un script permettant de découper une addresse IP en des sous réseaux en fonction
-    de nombre de sous réseaux et en fonction de nombre des machines.
+    Un script permettant de découper une addresse IP en des sous réseaux en
+    fonction de nombre de sous réseaux et en fonction de nombre des machines.
 '''
-import re #Module d'expression régulière en python
+
+
+import re  # Module d'expression régulière en python
 import math
 
-#Fonction de conversion de binaire en décimal
+
 def base2todec(bin):
-    cnv = 0
-    n = 0
+    ''' Fonction de conversion de binaire en décimal '''
+    cnv, n = 0, 0
     for i in bin[::-1]:
         cnv += int(i)*(2**n)
         n += 1
     return cnv
 
-#Fonction qui convertit le dernier octet de  masque en binaire
+
 def maskIso(mask):
+    ''' Fonction qui convertit le dernier octet de masque en binaire '''
     bt = list('00000000')
     val = mask % 8
     for i in range(val):
@@ -40,24 +43,29 @@ for i in range(int(nbSr)):
     print("")
     sR[name] = int(nbrPC)
 
+if sum(sR.values()) > 254:
+    print('Oupss! Le total des PCs est superieurs à 254, \nCe qui n\'est pas \
+possible pour un réseau de masque 24')
+    exit()
+
 sR = {k: v for k, v in sorted(sR.items(), key=lambda kv: kv[1], reverse=True)}
 # Triage par plus grand nombre de PC par sous-réseau d'où reverse.
 sRdec = {}
 lastIp = ipd
 for nom, nbr in sR.items():
-    n_s = math.log2(nbr+2) # On cherche d'abord n.
+    n_s = math.log2(nbr+2)  # On cherche d'abord n.
     if n_s - int(n_s) == 0:
         n = int(n_s)
     else:
         n = int(n_s) + 1
-    newmask = 32 - n # La formule de nouveau masque des sous réseaux.
+    newmask = 32 - n  # La formule de nouveau masque des sous réseaux.
     ipsr = lastIp
     sRdec[nom] = (ipsr, nbr, (newmask, maskIso(newmask)))
     lastIp = lastIp.split('.')
     lastIp[-1] = str(int(lastIp[-1]) + 2**n)
     lastIp = '.'.join(lastIp)
 
-for key, val in sRdec.items(): # Affichage des sous réseaux.
+for key, val in sRdec.items():  # Affichage des sous réseaux.
     print(f"""
         {key}: {val[1]} PCs
         --------------------------------------------
